305486dfcd3889e250e4705609066dded6a6e786
Calculatorhtml5 <singampallisuhruth1@gmail.com>
2025-10-14 19:33:00 -0400

    Delete src/main directory

--- src/main/java/com/example/superweapons/item/SuperweaponCoreItem.java
+++
@@ -1,9 +0,0 @@
-package com.example.superweapons.item;
-
-import net.minecraft.item.Item;
-
-public class SuperweaponCoreItem extends Item {
-    public SuperweaponCoreItem(Settings settings) {
-        super(settings);
-    }
-}
