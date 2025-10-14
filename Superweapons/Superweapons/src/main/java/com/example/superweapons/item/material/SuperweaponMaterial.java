f82c617d2e9d54d82c7140749b7a49fe3d6559b6
Calculatorhtml5 <singampallisuhruth1@gmail.com>
2025-10-14 19:17:14 -0400

    Add files via upload

---
+++ Superweapons/Superweapons/src/main/java/com/example/superweapons/item/material/SuperweaponMaterial.java
@@ -0,0 +1,40 @@
+package com.example.superweapons.item.material;
+
+import com.example.superweapons.Superweapons;
+import net.minecraft.item.ToolMaterial;
+import net.minecraft.recipe.Ingredient;
+
+import java.util.function.Supplier;
+
+public enum SuperweaponMaterial implements ToolMaterial {
+    SUPERWEAPON_MATERIAL(5, 2500, 12.0f, 6.0f, 20, () -> Ingredient.ofItems(Superweapons.SUPERWEAPON_CORE));
+
+    private final int miningLevel;
+    private final int itemDurability;
+    private final float miningSpeed;
+    private final float attackDamage;
+    private final int enchantability;
+    private final Supplier<Ingredient> repairIngredient;
+
+    SuperweaponMaterial(int miningLevel, int itemDurability, float miningSpeed, float attackDamage, int enchantability, Supplier<Ingredient> repairIngredient) {
+        this.miningLevel = miningLevel;
+        this.itemDurability = itemDurability;
+        this.miningSpeed = miningSpeed;
+        this.attackDamage = attackDamage;
+        this.enchantability = enchantability;
+        this.repairIngredient = repairIngredient;
+    }
+
+    @Override
+    public int getDurability() { return this.itemDurability; }
+    @Override
+    public float getMiningSpeedMultiplier() { return this.miningSpeed; }
+    @Override
+    public float getAttackDamage() { return this.attackDamage; }
+    @Override
+    public int getMiningLevel() { return this.miningLevel; }
+    @Override
+    public int getEnchantability() { return this.enchantability; }
+    @Override
+    public Ingredient getRepairIngredient() { return this.repairIngredient.get(); }
+}
